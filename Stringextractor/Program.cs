a7ebb766549368f0c3c2ae473ca2771453b3ad2c
JFromholtz <jfromholtz@hotmail.com>
2018-02-20 11:01:23 -0500

    initial commit

---
+++ Stringextractor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Stringextractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch bigtimer = new Stopwatch(); //timer for runtime statistics
+            bigtimer.Start();
+
+
+
+
+            bigtimer.Stop();
+            TimeSpan rundurationraw = bigtimer.Elapsed;
+
+            Console.WriteLine("Runtime " + rundurationraw);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
