1c712eac84d3958ed6f6a4b727b269075db3ffd2
JFromholtz <jfromholtz@hotmail.com>
2018-02-21 16:53:51 -0500

    first pass test

--- Stringextractor/Program.cs
+++ Stringextractor/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace Stringextractor
 {
@@ -9,7 +12,12 @@ namespace Stringextractor
         {
             Stopwatch bigtimer = new Stopwatch(); //timer for runtime statistics
             bigtimer.Start();
+            string root = Path.GetFullPath(@".\");
+            List<string> filelist = Directory.GetFileSystemEntries(root, "*", SearchOption.TopDirectoryOnly).ToList();
+            for(int count = 0; count < filelist.Count -1; count ++)
+            {
 
+            }
 
 
 
@@ -19,6 +27,22 @@ namespace Stringextractor
             Console.WriteLine("Runtime " + rundurationraw);
             Console.WriteLine("Press any key to exit");
             Console.ReadLine();
+        }
+
+        static void FormatHex(string Hex, string filepath)
+        {
+            string targetpath = Path.Combine(Path.GetDirectoryName(filepath), ("Stringsof" + Path.GetFileName(filepath) + ".txt"));
+            using (StreamWriter output = File.CreateText(targetpath))
+            {
+                output.WriteLine(Hex);
+            }
+        }
+
+        static string ReturnHex(string filepath)
+        {
+            byte[] bytes = File.ReadAllBytes(filepath);
+            string hex = BitConverter.ToString(bytes);
+            return hex.Replace("_", "");
         }
     }
 }
